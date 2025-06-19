80f9923813ec0828c07c8d8cf130fb482d42f590
Pedro Bruno <pbruno1283@gmail.com>
2025-06-19 17:35:03 -0300

    Dependency Injection Inserido o koin no projeto. Realizado setup inicial para começar a utilizar injeção de dependências.

---
+++ app/src/main/java/com/pedro/plataformachamados/MainApplication.kt
@@ -0,0 +1,20 @@
+package com.pedro.plataformachamados
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(level = Level.DEBUG)
+            androidContext(this@MainApplication)
+            modules()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/pedro/plataformachamados/di/AppModules.kt
@@ -0,0 +1,3 @@
+package com.pedro.plataformachamados.di
+
+
